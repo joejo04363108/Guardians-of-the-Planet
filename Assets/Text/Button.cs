b2161c8b5460c38b3129b0179ba07823ff1b620c
springyu <dofnstudy10212@gmail.com>
2024-12-30 22:35:48 +0800

    場景

---
+++ Assets/Text/Button.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+
+    // 按鈕點擊事件
+    public void TogglButton_1()
+    {
+         SceneManager.LoadScene("Start1");
+        
+    }
+
+    public void TogglButton_2()
+    {
+        Debug.Log("123");
+        
+    }
+    public void TogglButton_3()
+    {
+        SceneManager.LoadScene("Start0");
+        
+    }
+}
