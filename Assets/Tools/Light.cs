f0a4288c2a9ded0eb799dc295d08d6d8c2092466
springyu <dofnstudy10212@gmail.com>
2024-12-31 23:24:33 +0800

    增加useitem

---
+++ Assets/Tools/Light.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightBar : MonoBehaviour{
+
+	public Slider slider;
+
+	public void SetMaxLight(int health)
+	{
+		slider.maxValue = health;
+		slider.value = health;
+
+	}
+
+    public void SetLight(int health)
+	{		slider.value = health;
+
+	}
+	
+    public void addLight(int health)
+	{		slider.value += health;
+
+	}
+    public void subLight(int health)
+	{		slider.value -= health;
+
+	}
+	public float getLight(){
+		return slider.value;
+	}
+	
+	void Start(){
+		SetLight(60);
+	}
+
+}
